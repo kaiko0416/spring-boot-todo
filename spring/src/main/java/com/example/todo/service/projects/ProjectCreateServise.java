2a69b734801bfbd4a1dffb153c456f60df8eed27
kaiko <kaikm918@gmail.com>
2024-10-19 16:14:34 +0900

    add: ProjectCreateService を追加

---
+++ spring/src/main/java/com/example/todo/service/projects/ProjectCreateServise.java
@@ -0,0 +1,28 @@
+package com.example.todo.service.projects;
+
+import org.springframework.stereotype.Service;
+import com.example.todo.dto.request.ProjectCreateRequest;
+import com.example.todo.entity.Project;
+import com.example.todo.repository.ProjectRepository;
+
+@Service
+public class ProjectCreateServise {
+
+  private final ProjectRepository projectRepository;
+
+
+  public ProjectCreateServise(ProjectRepository projectRepository) {
+    this.projectRepository = projectRepository;
+  }
+
+
+
+  public Project invoke(ProjectCreateRequest request) {
+    Project project = new Project();
+
+    project.setName(request.getName());
+    project.setSummary(request.getSummary());
+
+    return this.projectRepository.save(project);
+  }
+}
