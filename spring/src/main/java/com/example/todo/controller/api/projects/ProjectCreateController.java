9795243377b1a1def62f3f176f0c539500ca71da
kaiko <kaikm918@gmail.com>
2025-01-25 14:12:21 +0900

    プロジェクトの追加機能を実装#1

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectCreateController.java
@@ -5,11 +5,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.example.todo.dto.request.ProjectCreateRequest;
+import com.example.todo.dto.request.projects.ProjectCreateRequest;
 import com.example.todo.entity.Project;
 import com.example.todo.service.projects.ProjectCreateService;
 import jakarta.validation.Valid;
 
+/**
+ * @RestController Spring MVCのコントローラとして振る舞うクラスに付与するアノテーション。
+ *                 このアノテーションが付与されたクラスは、HTTPリクエストを受け取るコントローラとして動作する。
+ *                 また、JSONやXMLなどのデータをHTTPレスポンスとして返却することができる。
+ *
+ * @RequestMapping クラスまたはメソッドに対してHTTPリクエストのパスやメソッドタイプ（GET、POSTなど）をマッピングするためのアノテーション。
+ *                 第一引数に、リクエストされるURLパスを指定する。
+ */
 @RestController
 @RequestMapping("/api/projects")
 public class ProjectCreateController {
@@ -20,6 +28,16 @@ public class ProjectCreateController {
     this.projectCreateService = projectCreateService;
   }
 
+  /**
+   * @PostMapping HTTP POSTリクエストを受け取るメソッドに付与するアノテーション。このアノテーションが付与されたメソッドは、HTTP
+   *              POSTリクエストを受け取るコントローラのメソッドとして動作する。
+   *
+   * @Valid メソッドの引数に付与するアノテーション。引数のオブジェクトに対してバリデーションを行う。
+   *        バリデーションエラーが発生した場合、メソッドの実行を中断し、エラーメッセージを返却する。このアノテーションを付与することで、コントローラ内でバリデーションを行うことができる。
+   *
+   * @RequestBody HTTPリクエストのボディ部分を受け取るメソッドの引数に付与するアノテーション。リクエストボディを受け取るために使用する。
+   *              リクエストボディとは、HTTPリクエストのボディ部分に含まれるJSONやXMLなどのデータのこと。
+   */
   @PostMapping
   public ResponseEntity<Project> invoke(@Valid @RequestBody ProjectCreateRequest request) {
     Project project = this.projectCreateService.invoke(request);
