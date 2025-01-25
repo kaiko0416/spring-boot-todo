f9ae37cc3b1470988f7399aa8ea8c6986bce20a3
kaiko <kaikm918@gmail.com>
2025-01-25 15:12:20 +0900

    プロジェクトの全件取得機能を実装#3

--- spring/src/main/java/com/example/todo/controller/api/projects/ProjectListController.java
+++ spring/src/main/java/com/example/todo/controller/api/projects/ProjectListController.java
@@ -22,6 +22,7 @@ public class ProjectListController {
   public ResponseEntity<List<Project>> invoke() {
     List<Project> projects = this.projectListService.invoke();
 
+
     return ResponseEntity.ok(projects);
   }
 }
