4b07dbaf660ca0e66d2c3523fcdea6de3c2243e6
kaiko <kaikm918@gmail.com>
2025-01-25 15:56:22 +0900

    タスクの一覧取得 #5

--- spring/src/main/java/com/example/todo/dto/request/tasks/TaskSearchRequest.java
+++ spring/src/main/java/com/example/todo/dto/request/tasks/TaskSearchRequest.java
@@ -1,5 +1,13 @@
 package com.example.todo.dto.request.tasks;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class TaskSearchRequest {
 
+  // プロジェクト単位で絞り込みをするために使用するパラメータ
+  private final Integer projectId;
 }
+
